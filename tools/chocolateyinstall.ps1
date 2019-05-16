1f1e89cc9d91ec71156d8db2bb161d184160f122
Tushar Joshi <tusharvjoshi@gmail.com>
2019-05-16 16:46:15 +0530

    Capitalize the hash code

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,20 +1,18 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://download.librebeans.org/download/CoolBeans-2018.12.2-x64.exe'
 $url64      = 'http://download.librebeans.org/download/CoolBeans-2018.12.2-x64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
   url64bit      = $url64
 
   softwareName  = 'coolbeans*'
 
-  checksum      = 'ad469cf2fe8e909e5e9c3c26e275d9e1d4552d527e5d63795165baf0d0459561'
-  checksumType  = 'sha256'
+  checksum64      = 'AD469CF2FE8E909E5E9C3C26E275D9E1D4552D527E5D63795165BAF0D0459561'
+  checksumType64  = 'sha256'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
